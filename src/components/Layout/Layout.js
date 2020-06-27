e36afd782012d18993b5dfdf3af64a78b35e0595
Adrian <adrian_zinko@op.pl>
2020-06-27 21:28:15 +0200

    feat: import components to layout base component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,50 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import Header from "../Header/Header"
+import Skills from "../Skills/Skills"
+import Projects from "../Projects/Projects"
+import Contact from "../Contact/Contact"
+
+import { fetchProjects } from "../../api/index"
+
+import classes from "./Layout.module.css"
+
+class Layout extends Component {
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchProjects()
+
+    console.log(fetchedData)
+
+    this.setState({ data: fetchedData })
+  }
+
+  render() {
+    const { data } = this.state
+
+    return (
+      <div className={classes.Container}>
+        <Header />
+        <Skills />
+        <Projects data={data} />
+        <Contact />
+      </div>
+    )
+  }
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
