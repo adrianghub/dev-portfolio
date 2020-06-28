3abe2f795e68eca2cb8ff8f4e2521238f18d63ec
Adrian <adrian_zinko@op.pl>
2020-06-28 16:38:13 +0200

    feat: set darkmode configuration

--- src/components/Navbar/NavItem/NavItem.js
+++ src/components/Navbar/NavItem/NavItem.js
@@ -3,12 +3,17 @@ import React, { useState } from "react"
 import "../../../scss/main.scss"
 
 const NavItem = ({ children, icon }) => {
-  const [darkmode, setDarkmode] = useState(false)
+  const [darkmode, setDarkmode] = useState(true)
 
   return (
     <li className="nav-item">
       <button className="icon-button" onClick={() => setDarkmode(!darkmode)}>
-        {darkmode ? (icon = "🌞") : (icon = "🌛")}
+        {darkmode
+          ? (document.body.classList.add("darkmode"))
+          : (document.body.classList.remove("darkmode"))}
+
+          {darkmode 
+          ? {icon} = '🌞' : {icon} = '🌛'}
       </button>
 
       {darkmode && children}
