82cbd54ec3f59c8b2ae56dde43a19f11ff6173ed
Adrian <adrian_zinko@op.pl>
2020-08-18 15:06:28 +0200

    feat: implement gatsby routing

--- src/components/Navbar/NavItems/NavItems.js
+++ src/components/Navbar/NavItems/NavItems.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
+import Link from "gatsby-link"
 
 import "../../../scss/main.scss"
 
@@ -13,11 +14,15 @@ const NavItem = ({ children, icon }) => {
 
   return (
     <>
-      <li  className="nav__item nav__item--blog">BLOG</li>
+      <Link to="/blog" className="nav__item">
+        <li className="nav__item nav__item--blog">BLOG</li>
+      </Link>
+      <Link to="/login" className="nav__item">
       <li className="nav__item nav__item--login">LOGIN</li>
+      </Link>
       <li className="nav__item nav__item--darkmode">
         <button className="icon__button" onClick={() => setDarkmode(!darkmode)}>
-          {darkmode ? ({icon} = "🌞") : ({icon} = "🌛")}
+          {darkmode ? ({ icon } = "🌞") : ({ icon } = "🌛")}
         </button>
         {darkmode && children}
       </li>
