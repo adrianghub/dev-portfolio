d1625bc69b8d0c6ceb3d01d8d1ce6dbec8856e31
Adrian <adrian_zinko@op.pl>
2020-08-29 12:39:13 +0200

    feat: connect project to firebase db

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCEulI7echOm4_H-oiaFTll8WjpzYir_hA",
+    authDomain: "portfolio-blog-ec32c.firebaseapp.com",
+    databaseURL: "https://portfolio-blog-ec32c.firebaseio.com",
+    projectId: "portfolio-blog-ec32c",
+    storageBucket: "portfolio-blog-ec32c.appspot.com",
+    messagingSenderId: "779653228014",
+    appId: "1:779653228014:web:11006a8019459c9c299aff",
+    measurementId: "G-M6NX2431JB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  firebase.firestore()
+
+  export default firebase;
