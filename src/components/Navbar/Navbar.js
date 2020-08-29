3572998fb9b3e7ed517bc54e71537fb09e1b02a1
Adrian <adrian_zinko@op.pl>
2020-08-29 14:54:27 +0200

    feat: add home icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,10 +3,14 @@ import Link from "gatsby-link"
 
 import "../../scss/main.scss"
 import logo from "../../images/logo.png"
+import HomeIcon from '@material-ui/icons/Home';
 
 const Navbar = ({ children }) => {
   return (
     <nav className="navbar">
+      <Link className="navbar__home--icon-wrapper" to="/">
+        <HomeIcon className="navbar__home--icon" />
+      </Link>
       <Link to="/">
         <img className="navbar__logo--image" src={logo} alt="Personal logo" />
       </Link>
