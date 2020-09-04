c1c63a13aef01164c552a0e3fa5628b72f15760c
Adrian <adrian_zinko@op.pl>
2020-09-04 22:46:08 +0200

    fix: correct a11y menu warnings

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,12 +9,18 @@ import CloseIcon from "@material-ui/icons/Close"
 const Navbar = ({ children }) => {
   const [click, setClick] = useState(false)
 
+  const handleKeyDown = (e) => {
+  if (e.keyCode === 13) {
+    setClick(!click)
+  }
+}
+
   return (
     <nav className="navbar">
         <Link to="/">
           <img className="navbar__logo--image" src={logo} alt="Personal logo" />
         </Link>
-        <div className="navbar__ham-menu" onClick={() => setClick(!click)}>
+        <div role="menu" tabIndex="0" className="navbar__ham-menu" onKeyDown={handleKeyDown} onClick={() => setClick(!click)}>
           {click ? (
             <CloseIcon className="navbar__menu-icon--close" />
           ) : (
