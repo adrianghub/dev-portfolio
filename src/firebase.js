d04e44a78676aff5adbace9c2400bcfe13187d65
Adrian <adrian_zinko@op.pl>
2020-09-08 17:30:24 +0200

    fix: correct firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -14,7 +14,7 @@ import firebase from 'firebase';
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
   const auth = firebase.auth();
-  const provider = new firebase.auth.GoogleAuthProvider
+  const provider = new firebase.auth.GoogleAuthProvider()
   
   export { auth, provider };
   export default db;
