505674da8b5449ba07b2772c8884e6e0cdc668af
Adrian <adrian_zinko@op.pl>
2020-09-08 17:37:40 +0200

    feat: enable google auth

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,8 +4,21 @@ import NavItems from "../components/Navbar/NavItems/NavItems"
 import "../scss/main.scss"
 import logoImage from "../images/logo.png"
 import { Button } from "@material-ui/core"
+import { auth, provider } from '../firebase';
 
 const login = () => {
+
+  const handleSignIn = () => {
+    auth
+    .signInWithPopup(provider)
+    .then(result => {
+      console.log(result);
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+  }
+
   return (
     <>
       <Navbar>
@@ -19,7 +32,7 @@ const login = () => {
             with exclusive access to my additional blog page content
           </h1>
           <p>zinko.adrian00@gmail.com</p>
-          <Button>Sign In with Google</Button>
+          <Button onClick={handleSignIn}>Sign In with Google</Button>
         </div>
       </div>
     </>
