9110374c22fed59805799c29eaa2b32f20048611
Adrian <adrian_zinko@op.pl>
2021-06-14 13:54:55 +0200

    fix: configurate missing canvas module

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,10 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
             test: /firebase/,
             use: loaders.null(),
           },
+          {
+            test: /canvas/,
+            use: loaders.null(),
+          },
         ],
       },
     })
